795ad297819927d1bdccda822e5931a448bdf875
K_Minhyeok <61306657+Kim-Min-Hyeok@users.noreply.github.com>
2024-03-17 16:36:06 +0900

    drag and drop
    아직 코드 연결 x

---
+++ 1st_hw_KimMinHyeok/1st_hw_KimMinHyeok/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  1st_hw_KimMinHyeok
+//
+//  Created by Minhyeok Kim on 2024/03/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
